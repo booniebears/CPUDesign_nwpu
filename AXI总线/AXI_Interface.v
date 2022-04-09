4a6938d826ce5c43cd1f0af812cff507cd419842
booniebears <3197571813@qq.com>
2022-04-10 01:20:55 +0800

    搭建了一个AXI基本框架

--- AXI总线/AXI_Interface.v
+++
@@ -1,7 +0,0 @@
-module AXI_Interface (
-    
-);
-
-//AXI控制模块 负责向AXI接口发送与接收数据/控制信号等
-
-endmodule //AXI_Interface
---
+++ with_AXI/AXICache_defines.vh
@@ -0,0 +1,4 @@
+`ifndef AXICACHE_DEFINES_VH
+    `define AXICACHE_DEFINES_VH
+
+`endif 
